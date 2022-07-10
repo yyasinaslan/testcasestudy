aa0381ac1c935a234323d64d532e41609db9dc51
YSN <yyasin.aslan@gmail.com>
2022-07-10 22:42:12 +0300

    Readme test

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
-## Test Case Study
+# Test Case Study
 
-### Kurulum
+## Kurulum
 
 - Kaynak kodlarını indirin:
-```bash
+
+```shell
 git clone https://github.com/yyasinaslan/ideasoftcasestudy.git
 ```
 
 - Gereken paketleri yükle
-```bash
+
+```shell
 composer install
 ```
 
-- Sunucuyu çalıştır:
-```bash
-docket compose up
+- Sunucuyu çalıştır\
+  **Not:** *Bu kod sunucuyu arka planda calistirir. Kapatmak icin [sunucu kapatma kodunu](#closer-server) kullanin*
+
+```shell
+docker compose up -d
 ```
+
+- Veritabanını ilk ayarlarını yap
+
+```shell
+php artisan migrate
+```
+
+## Sunucuyu kapatma {#close-server}
+
+```shell
+docker compose down
+```
+
---
+++ database/migrations/2022_07_10_192205_initial.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Orders table
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('consumerId');
+            $table->float('total');
+            $table->float('total');
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
