94f9d9bd9812834d5be2195eb2d9210ec013c226
YSN <yyasin.aslan@gmail.com>
2022-07-11 21:31:21 +0300

    crlf related errors for init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+if [ -d "/var/www/html/vendor" ]
+then
+    composer install
+else
+    echo "Vendor directory already exist. No need to install"
+fi
 
-# Gerekli paketleri yukluyoruz
-composer install
-
-# Veritabani tablolarini olusturuyoruz
 php artisan migrate:fresh
+
+/usr/local/bin/start-container
